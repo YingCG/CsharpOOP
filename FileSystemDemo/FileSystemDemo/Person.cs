1833051f1a7c78e53b2bf60bd0565a2a8b335d5e
YingCG <yingchiugoh@gmail.com>
2022-08-26 19:17:53 +1200

    read-write file, sending email, mongoDB with c#

---
+++ FileSystemDemo/FileSystemDemo/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileSystemDemo
+{
+    class Person
+    {
+        public Person(string firstName, string lastName, string URL)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.URL = URL;
+        }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string URL { get; set; }
+    }
+}
---
+++ FileSystemDemo/MongoDBDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDBDemo;
+
+namespace MongoDBDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string connectionString = "your connection string";
+            string databaseName = "simple_db";
+            string collectionName = "people";
+
+            var client = new MongoClient(connectionString);
+            var db = client.GetDatabase(databaseName);
+            var collection = db.GetCollection<PersonModel>(collectionName);
+
+            var person = new PersonModel { FirstName = "Timothy", LastName = "Grey" };
+            await collection.InsertOneAsync(person);
+
+            var results = await collection.FindAsync(_ => true);
+            foreach (var result in results.ToList())
+            {
+                Console.WriteLine(value: $"{result.Id}: {result.FirstName} {result.LastName}");
+            };
+        }
+    }
+}
