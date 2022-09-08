53f02962cba148612b15d0b1e18e2eafa5b61a52
YingCG <yingchiugoh@gmail.com>
2022-09-08 18:42:30 +1200

    using database with window form app

---
+++ MusicAppMongoDB/AlbumDB.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MongoDB.Driver;
+
+namespace MusicAppMongoDB
+{
+    class AlbumDB
+    {
+        private MongoClient _client;
+
+        public AlbumDB()
+        {
+            var setting = MongoClientSettings.FromConnectionString("Your Mongo Connection");
+            setting.ServerApi = new ServerApi(ServerApiVersion.V1);
+            _client = new MongoClient(setting);
+        }
+
+
+        public List<Album> GetAlbums()
+        {
+            return _client
+                .GetDatabase("music-app")
+                .GetCollection<Album>("albums")
+                .Find(FilterDefinition<Album>.Empty)
+                .ToList();
+        }
+    }
+}
---
+++ MusicAppMongoDB/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MusicAppMongoDB
+{
+    public partial class Form1 : Form
+    {
+        BindingSource albumBindingSource = new BindingSource();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AlbumDB albumsFromDB = new AlbumDB();
+            //Album artist1 = new Album
+            //{
+            //    AlbumName = "Album for coding",
+            //    ArtistName = "Some artist",
+            //    Year = 2022,
+            //    ImageURL = "nothing yet"
+            //};
+            //Album artist2 = new Album
+            //{
+            //    AlbumName = "Album for relaxing",
+            //    ArtistName = "Another artist",
+            //    Year = 2000,
+            //    ImageURL = "nothing yettt"
+            //};
+
+            //albumDAO.albums.Add(artist1);
+            //albumDAO.albums.Add(artist2);
+
+            // associate the list with the control (on top of form1)
+            albumBindingSource.DataSource = albumsFromDB.GetAlbums();
+
+            // connect the list to grid view
+            dataGridView1.DataSource = albumBindingSource;
+
+        }
+    }
+}
